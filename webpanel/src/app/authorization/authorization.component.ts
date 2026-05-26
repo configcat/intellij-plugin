2a1741c62c8266800e0a8b871d9985d8ea272463
novalisdenahi <novalisdenahi@gmail.com>
2026-05-26 10:28:16 +0200

    Web auth wip

---
+++ webpanel/src/app/authorization/authorization.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButton } from "@angular/material/button";
+import { AuthorizationComponent, AuthorizationModel } from "ng-configcat-publicapi-ui";
+import { AppData } from "../app-data";
+
+@Component({
+  selector: "configcat-intellij-authorization",
+  templateUrl: "./authorization.component.html",
+  styleUrls: ["./authorization.component.scss"],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatButton,
+    AuthorizationComponent,
+  ],
+})
+export class AuthComponent {
+  appData = inject(AppData);
+  loading = true;
+
+  login(authorizationParameters: AuthorizationModel) {
+    window["configCatAuthorizationSuccessMethod"].call(this, JSON.stringify(authorizationParameters));
+  }
+
+  unauthorize() {
+    window["configCatUnAuthorizationSuccessMethod"].call(this);
+  }
+
+}
